f8b3b2082da4c22f4b91046f938c5f6a6034cd3b
Johan Wassberg <jocar@sunet.se>
2025-12-02 16:09:05 +0100

    Use converter from django-ca to be compatible with Django 6.0
    Closes #16

--- django_ca_cmc/urls.py
+++ django_ca_cmc/urls.py
@@ -7,5 +7,5 @@ from django_ca_cmc import views
 app_name = "django_ca_cmc"
 
 urlpatterns: list[URLResolver | URLPattern] = [
-    path("<serial:serial>/", views.CMCView.as_view(), name="cmc"),
+    path("<django-ca-serial:serial>/", views.CMCView.as_view(), name="cmc"),
 ]
